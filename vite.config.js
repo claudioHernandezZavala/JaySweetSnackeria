f56b03e5d7e8cf79b41914d6b2222d443093312c
Claudio Hernandez <claudioherza@gmail.com>
2026-04-24 00:22:26 -0400

    a

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const githubPagesBase = "/snackeria/";
+const githubPagesBase = "/JaySweetSnackeria/";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
